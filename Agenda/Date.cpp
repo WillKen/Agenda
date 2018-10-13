417495520250d3945853474d2e4a1ad5bd5000d7
zhangweikun <501352226@qq.com>
2018-10-13 16:27:33 +0800

    improve

--- Agenda/Date.cpp
+++ Agenda/Date.cpp
@@ -9,7 +9,7 @@ Date::Date()
 }
 
 
-Date(int t_year, int t_month, int t_day, int t_hour, int t_minute)
+Date::Date(int t_year, int t_month, int t_day, int t_hour, int t_minute)
 {
     m_year=t_year;
     m_month=t_month;
@@ -18,7 +18,7 @@ Date(int t_year, int t_month, int t_day, int t_hour, int t_minute)
     m_minute=t_minute;
 }
 
-Date(std::string dateString)
+Date::Date(std::string dateString)
 {
 
 }
@@ -32,53 +32,55 @@ int Date::getYear(void) const
   * @brief set the year of a date
   * @param a integer indicate the new year of a date
   */
-  void setYear(const int t_year);
+void Date::setYear(const int t_year) {
+
+  }
 
   /**
   * @brief return the month of a Date
   * @return   a integer indicate the month of a date
   */
-int Meeting::getMonth(void) const
+int Date::getMonth(void) const
 {
     return m_month;
 }
 
-void Meeting::setMonth(const int t_month)
+void Date::setMonth(const int t_month)
 {
     m_month=t_month;
 }
 
-int Meeting::getDay(void) const
+int Date::getDay(void) const
 {
     return m_day;
 }
 
-void setDay(const int t_day)
+void Date::setDay(const int t_day)
 {
     m_day=t_day;
 }
 
-int Meeting::getHour(void) const
+int Date::getHour(void) const
 {
     return m_hour;
 }
 
-void Meeting::setHour(const int t_hour)
+void Date::setHour(const int t_hour)
 {
     m_hour=t_hour;
 }
 
-int Meeting::getMinute(void) const
+int Date::getMinute(void) const
 {
     return m_minute;
 }
 
-void Meeting::setMinute(const int t_minute)
+void Date::setMinute(const int t_minute)
 {
     m_minute=t_minute;
 }
 
-static bool Meeting::isValid(const Date t_date)
+bool Date::isValid(const Date t_date)
 {
 
 }
@@ -87,47 +89,51 @@ static bool Meeting::isValid(const Date t_date)
   * 0000-00-00/00:00
   * @return a date
   */
-  static Date stringToDate(const std::string t_dateString);
+Date Date::stringToDate(const std::string t_dateString)
+{
+
+}
 
   /**
   * @brief convert a date to string, if the format is not correct return
   * 0000-00-00/00:00
   */
-  static std::string dateToString(Date t_date);
+std::string Date::dateToString(Date t_date)
+{
+
+}
+
+
 
   /**
   *  @brief overload the assign operator
   */
-  Date &operator=(const Date &t_date);
+Date & Date::operator=(const Date & t_date)
+{
+	// TODO: 在此处插入 return 语句
+}
 
-  /**
-  * @brief check whether the CurrentDate is equal to the t_date
-  */
-  bool operator==(const Date &t_date) const;
+bool Date::operator==(const Date & t_date) const
+{
+	return false;
+}
 
-  /**
-  * @brief check whether the CurrentDate is  greater than the t_date
-  */
-  bool operator>(const Date &t_date) const;
+bool Date::operator>(const Date & t_date) const
+{
+	return false;
+}
 
-  /**
-  * @brief check whether the CurrentDate is  less than the t_date
-  */
-  bool operator<(const Date &t_date) const;
+bool Date::operator<(const Date & t_date) const
+{
+	return false;
+}
 
-  /**
-  * @brief check whether the CurrentDate is  greater or equal than the t_date
-  */
-  bool operator>=(const Date &t_date) const;
+bool Date::operator>=(const Date & t_date) const
+{
+	return false;
+}
 
-  /**
-  * @brief check whether the CurrentDate is  less than or equal to the t_date
-  */
-  bool operator<=(const Date &t_date) const;
-/*
-private:
-  int m_year;
-  int m_month;
-  int m_day;
-  int m_hour;
-  int m_minute;*/
+bool Date::operator<=(const Date & t_date) const
+{
+	return false;
+}
--- Agenda/User.cpp
+++ Agenda/User.cpp
@@ -5,58 +5,58 @@ User::User()
 
 }
 
-User::User(std::string t_userName, std::string t_userPassword,std::string t_userEmail, std::string t_userPhone)
+User::User(std::string t_userName, std::string t_userPassword, std::string t_userEmail, std::string t_userPhone)
 {
-    m_name=t_userName;
-    m_password=t_userPassword;
-    m_email=t_userEmail;
-    m_phone=t_userPhone;
+	m_name = t_userName;
+	m_password = t_userPassword;
+	m_email = t_userEmail;
+	m_phone = t_userPhone;
 }
 
 User::User(const User &t_user)
 {
-    m_name=t_user.getName();
-    m_password=t_user.getPassword();
-    m_email=t_user.getEmail();
-    m_phone=t_user.getPhone();_
+	m_name = t_user.getName();
+	m_password = t_user.getPassword();
+	m_email = t_user.getEmail();
+	m_phone = t_user.getPhone();
 }
 
 std::string User::getName() const
 {
-    return m_name;
+	return m_name;
 }
 
-void setName(std::string t_name)
+void User::setName(std::string t_name)
 {
-    m_name=t_name;
+	m_name = t_name;
 }
 
 std::string User::getPassword() const
 {
-    return m_password;
+	return m_password;
 }
 
-void setPassword (std::string t_password)
+void User::setPassword(std::string t_password)
 {
-    m_password=t_password;
+	m_password = t_password;
 }
 
 std::string User::getEmail() const
 {
-    return m_email;
+	return m_email;
 }
 
-void setEmail(std::string t_email)
+void User::setEmail(std::string t_email)
 {
-    m_email=t_email;
+	m_email = t_email;
 }
 
 std::string User::getPhone() const
 {
-    return m_phone;
+	return m_phone;
 }
 
-void setPhone(std::string t_phone)
+void User::setPhone(std::string t_phone)
 {
-    m_phone=t_phone;
+	m_phone = t_phone;
 }
